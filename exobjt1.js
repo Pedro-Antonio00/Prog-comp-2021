b7bb662fa3fda45d824d825dc1ac03dc2d01b920
Pedro-Antonio <pedrohiuga1500@gmail.com>
2021-08-18 11:07:42 -0300

    18-8

--- exobjt1.js
+++ exobjt1.js
@@ -1,42 +1,49 @@
 let genredes = () => {
     let redevet = []
-    let posts = []
     for(let i=0;i<2;i++){
         let redes = {
             codigo: Number(prompt(`Informe o código da rede social`)),
             nome: prompt(`Digite o nome da rede social`),
             link: prompt(`Insira o link da rede social`)
         }
-        posts[i] = 0
         redevet.push(redes)
     }
     let usuvet = []
-    let ptotal
-    for(let i=0;i<2;i++){
+    let i = 0
+    while(i<2){
         let usuarios = {
             login: prompt(`Informe o login do usuario`),
             nome: prompt(`Informe o nome do usuario`),
-            codr: Number(prompt(`Informe o código de rede social`)),
+            codigorede: Number(prompt(`Informe o código de rede social`)),
             qtdp: Number(prompt(`Informe a quantidade de posts do usuario`))
         }
-        let v = false
-        let cont = 0
-        while(v!=true && cont<2){
-            if(redevet[cont].codigo==usuarios.codr){
+        let achou = false
+        let j = 0
+        while(!achou && j<2){
+            if(redevet[j].codigo==usuarios.codigorede){
                 usuvet.push(usuarios)
-                posts[cont] = usuarios.qtdp + posts[cont]
-                ptotal = ptotal + usuarios.qtdp
-                v = true
+                achou = true
                 alert(`Usuario inserido com sucesso`)
             }
-            cont++
+            j++
         }
-        if(v==false){
+        if(!achou){
             alert(`codigo invalido`)
-            i--
         }
-        
+        i++
     }
-    let loginu = prompt(`Informe o login do usuario`)
-    alert(``)
+    let codigo = Number(prompt(`Informe o codigo da rede social`))
+    let conta = 0
+    
+    for(let i=0;i<5;i++){
+        if(usuvet[i].codigorede == codigo){
+            conta = conta + usuvet[i].qtdp
+        }
+    }
+    if(conta==0){
+        console.log(`Não houve postagem ou a rede social não existe`)
+    }
+    else{
+        console.log(`A quantidade de posts na rede social ${codigo} foi ${conta}`)
+    }
 }
